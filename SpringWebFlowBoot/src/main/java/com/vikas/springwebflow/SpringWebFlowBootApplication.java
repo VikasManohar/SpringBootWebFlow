4b250a0597a412b6cc16f8c04a7768c74f611690
VikasManohar <vikasmanohar.1995@gmail.com>
2019-09-29 16:11:21 +0530

    SpringBoot with webFlow

---
+++ SpringWebFlowBoot/src/main/java/com/vikas/springwebflow/SpringWebFlowBootApplication.java
@@ -0,0 +1,13 @@
+package com.vikas.springwebflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFlowBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFlowBootApplication.class, args);
+	}
+
+}
